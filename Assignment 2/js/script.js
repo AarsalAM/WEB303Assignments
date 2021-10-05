668db81df82d8dd41973a8a3993b99af777eef86
AarsalM <91852812+AarsalM@users.noreply.github.com>
2021-10-05 01:02:56 -0400

    Finished Assignment 2, already tested on scweb

--- Assignment 2/js/script.js
+++ Assignment 2/js/script.js
@@ -1,3 +1,16 @@
 // WEB303 Assignment 2
-// Your Name Here
+// Aarsal Masoodi
 
+$(document).ready(function() {
+    $('#vprospect').click(function() {
+        $('#solution').load("prospect.html");
+    });
+
+    $('#vconvert').click(function() {
+        $('#solution').load("convert.html");
+    });
+
+    $('#vretain').click(function() {
+        $('#solution').load("retain.html");
+    });
+});
