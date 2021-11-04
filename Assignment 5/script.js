cdf67aa34eaa46d25e0e314dcb4c0cef0d4ad5f4
AarsalM <91852812+AarsalM@users.noreply.github.com>
2021-11-03 23:12:48 -0400

    Finished the updateContent method. Working on toString.

--- Assignment 5/script.js
+++ Assignment 5/script.js
@@ -16,7 +16,29 @@ $(document).ready(function(){
             this.description = description;
             this.categoryType = categoryType;
         }
+
+        updateContent(title, description, categoryType) {
+            if ((this.title != null) || (this.description != null) || (this.categoryType != null)) {
+                console.log("The title is not null");
+                this.title = title;
+                this.description = description;
+                this.categoryType = categoryType;
+            }
+        }
+        
+        toString() {
+            return "<h1>the title is: " + this.title + "</h1>";
+        }
     }
+
+    let movie1 = new ContentCard(1, "Distict 9", "fun movie", "scifi");
+    let movstring = `${movie1}`;
+
+    $("#content-list").append(movstring);
+    
+    console.log(movie1);
+    movie1.updateContent("","","fantasy");
+    console.log(movie1);
 
 });
 
