b0f290b65d51e0dd7c9345b052cad04550ec006a
AarsalM <aarsal.masoodi10@stclairconnect.ca>
2021-11-11 12:00:53 -0500

    Cleaned up script, tried getting the accordion toggle to work.

--- Assignment 6/style.js
+++ Assignment 6/style.js
@@ -1,8 +1,6 @@
 $(function() {
     $('.accordion').on('click', '.accordion-control', function(e) {
-        console.log("Event data", e);
         e.preventDefault();
-        console.log("this is ", this);
         $(this)
             .toggleClass('active')
             .next('.accordion-panel')
